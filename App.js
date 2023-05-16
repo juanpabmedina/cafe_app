ba8cf92c0b290c6a0d9fda0be871d378140b3867
juanpabmedina <juanpab.m182@gmail.com>
2023-05-16 17:41:38 -0500

    added new icons and set time for flash screen

--- App.js
+++ App.js
@@ -5,10 +5,22 @@ import Fermentation_station from './src/screens/fermentation_station'
 import Navigation from './src/navigation/Navigator'
 import { ImageBackground, StyleSheet, ScrollView } from 'react-native'
 import SplashScreen from 'react-native-splash-screen'
+
+
 const App = () => {
-  useEffect(() => {
-    SplashScreen.hide();
-  }, [])
+
+  const [hideSplash, setHideSplash] = React.useState(false);
+
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setHideSplash(true);
+    }, 5000); // amount of time the splash is shown from the time component is rendered
+  }, []);
+  
+  React.useEffect(() => {
+    hideSplash && SplashScreen.hide();
+  }, [hideSplash]);
   return(
     <ImageBackground source={require("./src/images/background_app.png")} resizeMode="cover" style={styles.background}>
      
